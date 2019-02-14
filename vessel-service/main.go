df1a7c36924795ade87191d9c184c40e2dd2198a
terry.shi <shidakang@yundianjia.com>
2019-02-14 17:44:25 +0800

    update

--- vessel-service/main.go
+++ vessel-service/main.go
@@ -1,66 +1,125 @@
-// 货船服务  根据集装箱的重量，数量去寻找合适的货运船
+//// 货船服务  根据集装箱的重量，数量去寻找合适的货运船
+//package main
+//
+//import (
+//	"errors"
+//	"fmt"
+//	//与本地路径一致 dockerfile中workdir也要与此一致
+//	pb "github.com/terryshi96/shippy/vessel-service/proto"
+//	"github.com/micro/go-micro"
+//	"context"
+//)
+//
+//// 定义Model
+//type VesselRepository struct {
+//	vessels []*pb.Vessel
+//}
+//
+//// 定义model类方法
+//type Repository interface {
+//	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+//}
+//
+//// 实现model类方法
+//// FindAvailable - 根据Specification，从若干货船中挑选出合适的货船来运送货物
+//// 如果货物的数量和重量都没有超过一个货船的数量和重量上限，
+//// 那么我们就返回这个货船
+//func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+//	// 遍历所有的货船
+//	for _, vessel := range repo.vessels {
+//		// 返回数量和重量上限量大于需求的货船
+//		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+//			return vessel, nil
+//		}
+//	}
+//	return nil, errors.New("No vessel found by that spec")
+//}
+//
+//// 定义controller，在controller方法中调用model类方法
+//type service struct {
+//	repo Repository
+//}
+//
+//func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+//	vessel, err := s.repo.FindAvailable(req)
+//	if err != nil {
+//		return err
+//	}
+//	res.Vessel = vessel
+//	return nil
+//}
+//func main() {
+//
+//	// 创建一套货船记录 后续使用数据库代替
+//	vessels := []*pb.Vessel{
+//		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+//	}
+//	repo := &VesselRepository{vessels}
+//	srv := micro.NewService(
+//		micro.Name("go.micro.srv.vessel"),
+//		micro.Version("latest"),
+//	)
+//	srv.Init()
+//	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+//	if err := srv.Run(); err != nil {
+//		fmt.Println(err)
+//	}
+//}
 package main
 
 import (
-	"errors"
 	"fmt"
-	//与本地路径一致 dockerfile中workdir也要与此一致
+	"log"
+	"github.com/terryshi96/shippy/common"
 	pb "github.com/terryshi96/shippy/vessel-service/proto"
 	"github.com/micro/go-micro"
-	"context"
+	"os"
 )
 
-// 定义Model
-type VesselRepository struct {
-	vessels []*pb.Vessel
-}
 
-// 定义model类方法
-type Repository interface {
-	FindAvailable(*pb.Specification) (*pb.Vessel, error)
-}
+const (
+	defaultHost = "localhost:27017"
+)
 
-// 实现model类方法
-// FindAvailable - 根据Specification，从若干货船中挑选出合适的货船来运送货物
-// 如果货物的数量和重量都没有超过一个货船的数量和重量上限，
-// 那么我们就返回这个货船
-func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
-	// 遍历所有的货船
-	for _, vessel := range repo.vessels {
-		// 返回数量和重量上限量大于需求的货船
-		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
-			return vessel, nil
-		}
+func createDummyData(repo Repository) {
+	defer repo.Close()
+	vessels := []*pb.Vessel{
+		{Id: "vessel001", Name: "Kane's Salty Secret", MaxWeight: 200000, Capacity: 500},
 	}
-	return nil, errors.New("No vessel found by that spec")
-}
-
-// 定义controller，在controller方法中调用model类方法
-type service struct {
-	repo Repository
-}
-
-func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
-	vessel, err := s.repo.FindAvailable(req)
-	if err != nil {
-		return err
+	for _, v := range vessels {
+		repo.Create(v)
 	}
-	res.Vessel = vessel
-	return nil
 }
+
 func main() {
 
-	// 创建一套货船记录 后续使用数据库代替
-	vessels := []*pb.Vessel{
-		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+	host := os.Getenv("DB_HOST")
+
+	if host == "" {
+		host = defaultHost
 	}
-	repo := &VesselRepository{vessels}
+
+	session, err := common.CreateSession(host)
+	defer session.Close()
+
+	if err != nil {
+		log.Fatalf("Error connecting to datastore: %v", err)
+	}
+
+	repo := &VesselRepository{session.Copy()}
+
+	createDummyData(repo)
+
 	srv := micro.NewService(
 		micro.Name("go.micro.srv.vessel"),
 		micro.Version("latest"),
 	)
+
 	srv.Init()
-	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+
+	// Register our implementation with
+	pb.RegisterVesselServiceHandler(srv.Server(), &service{session})
+
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
 	}
